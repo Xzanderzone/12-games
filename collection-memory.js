165332909edc5aad0c294d7c98d38e64e9588350
PIeter <pieter_ophalvens@hotmail.com>
2023-11-11 09:42:18 +0100

    added 3 of a kind dificulty 10-18 and 0 and graying out correct pairs

--- collection-memory.js
+++ collection-memory.js
@@ -1,24 +1,33 @@
 let suits = ["♠", "♦️", "♣️", "♥","•","○","♂","♀","©"];
+let color = ["black", "red", "black", "red","green","blue","green","blue","purple"];
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let difficultyLevel=1; 
 export function SetDifficulty(difficulty)
 {
-	if(difficulty>=1)difficultyLevel=difficulty;
+	difficultyLevel=difficulty;
 }
 	export function getDeck()
 	{
 	let deck = [];
-	let insane=0;
-	// if(difficultyLevel>4)insane+=difficultyLevel-4;
-	for (let double=0;double<2;double++)
+	let pairs;
+	if(difficultyLevel<=9)pairs=2;
+	else if(difficultyLevel>9)pairs=3;
+	for (let duplicate=0;duplicate<pairs;duplicate++)
 	{
+		let maxsuits=suits.length;
+		if (difficultyLevel>maxsuits)difficultyLevel-=maxsuits;
 		for(let i = 0; i < suits.length && i<difficultyLevel; i++)
 		{
 			for(let x = 0; x < values.length; x++)
 			{
-				let card = {Value: values[x], Suit: suits[i],useless:double};
+				let card = {Value: values[x], Suit: suits[i],color:color[i]};
 				deck.push(card);
 			}
+		}
+		if(difficultyLevel==0){
+			let randomcard=Math.floor(Math.random()*maxsuits);
+			let card = {Value: values[0], Suit: suits[randomcard],color:color[0]};
+			deck.push(card);
 		}
 	}
 	return deck;
--- memory-game.js
+++ memory-game.js
@@ -1,13 +1,18 @@
 import {getDeck,shuffle,SetDifficulty} from "./collection-memory.js"
 
 let deck=[];
-let card1=false,card2=false;
+let card1=false,card2=false,card3=false;
+let difficulty=1;
 let main=document.body.querySelector("main");
 let difficultyswitch=document.body.querySelectorAll(".difficultyswitch");
 difficultyswitch.forEach(element => {
   element.addEventListener("click",(e)=>{
     e.preventDefault();  
-    SetDifficulty(parseInt(e.target.textContent));
+    difficulty=parseInt(e.target.textContent);
+    SetDifficulty(difficulty);
+    card1=false;
+    card2=false;
+    card3=false;
     Start();
     });
 });
@@ -22,7 +27,7 @@ function Start(){
     let card=document.createElement("button");
     let cardtitle=document.createElement("p");
     card.classList.add("back");
-    if(deck[i].Suit==="♦️"||deck[i].Suit=== "♥")card.style.color="red";
+    card.style.color=deck[i].color;
     cardtitle.textContent=deck[i].Value+deck[i].Suit;
     card.addEventListener("click",function(e){
       if(card1==false){
@@ -34,13 +39,36 @@ function Start(){
         this.classList.add("front");
         this.classList.remove("back");
         card2=this;
-        if(card1.children[0].textContent==card2.children[0].textContent && card1!=card2){
+        if(card1==card2)card2=false;
+        else if(card1.children[0].textContent==card2.children[0].textContent && card1!=card2 && difficulty<=9){
           card1.disabled=true;
+          card1.style.color="gray";
           card1=false;
           card2.disabled=true;
+          card2.style.color="gray";
           card2=false;
         }
-        else setTimeout(hideCards,1000);
+        else if(difficulty<=9)setTimeout(hideCards,1000);
+      }
+      else if(card3==false &&difficulty>9){
+        this.classList.add("front");
+        this.classList.remove("back");
+        card3=this;
+        if(card1==card3)card3=false;
+        else if(card2==card3)card3=false;
+        else if(card1.children[0].textContent==card2.children[0].textContent && card1!=card2 && difficulty>9
+          &&card2.children[0].textContent==card3.children[0].textContent&& card2!=card3 &&card1!=card3){
+          card1.disabled=true;
+          card1.style.color="gray";
+          card1=false;
+          card2.disabled=true;
+          card2.style.color="gray";
+          card2=false;
+          card3.disabled=true;
+          card3.style.color="gray";
+          card3=false;
+        }
+        else if(difficulty>9)setTimeout(hideCards,1000);
       }
     })
     card.appendChild(cardtitle);
@@ -51,8 +79,13 @@ function Start(){
 function hideCards(){
   card1.classList.remove("front");
   card1.classList.add("back");
+  card1=false;
   card2.classList.remove("front");
   card2.classList.add("back");
-  card1=false;
   card2=false;
+  if(card3!=false){
+    card3.classList.remove("front");
+    card3.classList.add("back");
+    card3=false;
+  }
 }
